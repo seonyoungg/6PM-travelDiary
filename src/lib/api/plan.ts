9414b5783dc8d1e88547168d0aeb1e5948700fb6
seonyoungg <tjsduddlfma1@naver.com>
2025-09-04 20:25:48 +0900

    ♻️ refactor: API_URL 및 CLIENT_ID 호출 분리

--- src/lib/api/plan.ts
+++ src/lib/api/plan.ts
@@ -1,8 +1,6 @@
+import { API_URL, CLIENT_ID } from "@/lib/api/apiConfig";
 import { ApiResPromise } from "@/types/api";
 import { GetPlanDetailProps } from "@/types/plan";
-
-const API_URL = process.env.NEXT_PUBLIC_API_SERVER;
-const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
 
 /**
  * 사용자가 작성한 여행 계획(type=plan) 게시물 목록을 조회
@@ -10,9 +8,6 @@ const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
  * @returns {Promise<ApiRes<GetPlanDetailProps[]>>} 여행 계획 게시물 배열을 포함한 응답 객체
  */
 export async function getPlanListUser(token: string | null): ApiResPromise<GetPlanDetailProps[]> {
-  if (!API_URL) throw new Error("API_URL이 설정되어 있지 않습니다.");
-  if (!CLIENT_ID) throw new Error("CLIENT_ID이 설정되어 있지 않습니다.");
-
   try {
     const res = await fetch(`${API_URL}/posts/users?type=plan`, {
       method: "GET",
