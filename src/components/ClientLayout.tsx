c31a84c91d974e30da9c388668f5f1ccf1a8b067
선영 <tjsduddlfma1@naver.com>
2025-09-18 17:12:15 +0900

    💄 style: 코드포맷팅설정(Prettier)

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -99,7 +99,7 @@ function IntroScreen({ onFinish }: { onFinish: () => void }) {
             className="flex justify-center mt-12"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ 
+            transition={{
               delay: 0.8,
               duration: 0.4,
             }}
